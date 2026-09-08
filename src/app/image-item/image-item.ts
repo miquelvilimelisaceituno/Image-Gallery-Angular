ec44cbdf5bed3c957a798c720ac50e5514949238
Miquel <miquel.vilimelis@gmail.com>
2026-09-08 10:15:04 +0200

    Added Gallery and Image-item
    Punto 4 de la lista de tareas, simples comandos "ng generate component" ejecutados

---
+++ src/app/image-item/image-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-image-item',
+  styleUrl: './image-item.css',
+  templateUrl: './image-item.html',
+})
+export class ImageItem {}
