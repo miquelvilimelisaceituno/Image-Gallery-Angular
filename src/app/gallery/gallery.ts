706fb7ee7844e9863a7fe228dd6e4ddcfd41ce90
Miquel <miquel.vilimelis@gmail.com>
2026-09-08 13:49:21 +0200

    Added to the gallery and item-image
    Adapted the tasks to the most moder version of angular (there are no ".compnent." Trying to understant the steps.

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { Image } from '../interfaces/image-interface';
+import { ImageItem } from '../image-item/image-item';
 
 @Component({
-  imports: [],
+  imports: [ImageItem],
   selector: 'app-gallery',
   styleUrl: './gallery.css',
   templateUrl: './gallery.html',
+  
 })
-export class Gallery {}
+export class Gallery {
+  images: Image[] = [
+  { id: '1', src: 'https://picsum.photos/id/237/400/300', alt: 'Perro negro tumbado' },
+  { id: '2', src: 'https://picsum.photos/id/238/400/300', alt: 'Ciudad en blanco y negro' },
+  { id: '3', src: 'https://picsum.photos/id/239/400/300', alt: 'Diente de león' },
+  { id: '4', src: 'https://picsum.photos/id/240/400/300', alt: 'Escaleras que van al mar' },
+];
+}
--- src/app/image-item/image-item.ts
+++ src/app/image-item/image-item.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import { Image } from '../interfaces/image-interface';
 
 @Component({
   imports: [],
   selector: 'app-image-item',
   styleUrl: './image-item.css',
   templateUrl: './image-item.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ImageItem {}
+export class ImageItem {
+  image = input.required<Image>()
+}
