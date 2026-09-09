5b38a7f3fdc14f48eb19d267ddcf442310d1ff26
Miquel <miquel.vilimelis@gmail.com>
2026-09-09 17:01:35 +0200

    Integrated Gallery to the componnent app
    Also added a hover over the css so the transform can be checked.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Gallery } from './gallery/gallery';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Gallery],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
